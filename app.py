b4765c4bcbe764a68d297d3c7c3db832618fbe6a
UvanShankar <47663272+UvanShankar@users.noreply.github.com>
2020-06-06 17:06:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,13 +18,8 @@ app.secret_key = 'uvanshankar'
 api = Api(app)
 
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
 
 jwt = JWT(app, authenticate, identity)  # /auth
-
 
 api.add_resource(UserRegister, '/register')
 api.add_resource(SampleModel, '/model')
@@ -33,4 +28,10 @@ api.add_resource(SampleModel, '/model')
 if __name__ == '__main__':
     from db import db
     db.init_app(app)
-    app.run(port=5000, threaded=False, debug=False)
+
+    if app.config['DEBUG']:
+        @app.before_first_request
+        def create_tables():
+            db.create_all()
+
+    app.run(port=5000)
